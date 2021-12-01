1b406523ed9871630b51bf7eb14e843f0bb46117
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2021-12-01 21:03:46 +0300

    today

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
 function update() {
 	let time = document.getElementById('time');
+	let myDate = document.getElementById('date');
+	let week = document.getElementById('week');
+	
 	let date = new Date();
 	let hours = date.getHours();
 	if (hours < 10) hours = '0' + hours;
@@ -9,6 +12,22 @@ function update() {
 	let minutes = date.getMinutes();
 	if (minutes < 10) minutes = '0' + minutes;
 	time.children[1].innerHTML = minutes;
+
+	let curDate = date.getDate();
+	if (curDate < 10) curDate = '0' + curDate;
+	myDate.children[0].innerHTML = curDate;
+
+	let month = date.getMonth();
+	if (month < 10) month = '0' + month;
+	myDate.children[1].innerHTML = month + 1;
+
+	let year = date.getFullYear();
+	if (year < 10) year = '0' + year;
+	myDate.children[2].innerHTML = year;
+
+	let day = date.getDay();
+	week.children[day].classList.add('current-day');
+
 
 	Object.keys(x).forEach(function(el) {
 		let xEnd = String(x[el].timeEnd);
