8cef3c023b4cc30f620f4848cf1892938808b902
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2021-12-06 07:13:20 +0300

    week

--- js/script.js
+++ js/script.js
@@ -1,73 +1,81 @@
-
 function update() {
+	let date = new Date();
 	let time = document.getElementById('time');
 	let myDate = document.getElementById('date');
 	let week = document.getElementById('week');
-	let date = new Date();
+	
+	updateHours();
+	updateMinutes();
+	updateDate();
+	updateMonth();
+	updateYear();
+	updateWeek();
+	updateTest();
 
-	let hours = date.getHours();
-	if (hours < 10) hours = '0' + hours;
-	if (hours != time.children[0].outerText) {
-		time.children[0].innerHTML = hours;
-	}
-
-	let minutes = date.getMinutes();
-	if (minutes < 10) minutes = '0' + minutes;
-	if (minutes != time.children[1].outerText) {
-		time.children[1].innerHTML = minutes;
-	}
-
-	let curDate = date.getDate();
-	if (curDate < 10) curDate = '0' + curDate;
-	if (curDate != myDate.children[0].outerText) {
-		myDate.children[0].innerHTML = curDate;
-	}
-
-	let month = date.getMonth() + 1;
-	if (month < 10) month = '0' + month;
-	if (month != myDate.children[1].outerText) {
-		myDate.children[1].innerHTML = month;
-	}
-
-	let year = date.getFullYear();
-	if (year < 10) year = '0' + year;
-	if (year != myDate.children[2].outerText) {
-		myDate.children[2].innerHTML = year;
-	}
-
-	let day = date.getDay();
-	if (day > 0) {
-		if (day === 1) {
-			week.children[(day - 1)].classList.add('current-day');
-			week.children[(day + 5)].classList.remove('current-day');
-		}else if(day > 1){
-			week.children[(day - 1)].classList.add('current-day');
-			week.children[(day - 2)].classList.remove('current-day');
+	function updateHours(){
+		let hours = date.getHours();
+		if (hours < 10) hours = '0' + hours;
+		if (hours != time.children[0].outerText) {
+			time.children[0].innerHTML = hours;
 		}
-		else{
-			week.children[(day - 1)].classList.add('current-day');
-		};
-	}else{
-		week.children[(day + 6)].classList.add('current-day');
-		week.children[(day + 5)].classList.remove('current-day');
 	}
-	// let day = date.getDay();
-	// week.children[day].classList.add('current-day');
-	// if (day > 0) {
-	// 	week.children[(day - 1)].classList.remove('current-day');
-	// }else{
-	// 	week.children[(day + 6)].classList.remove('current-day');
-	// }
 
-
-
-	Object.keys(x).forEach(function(el) {
-		let xEnd = String(x[el].timeEnd);
-		let currentData = document.getElementById("test-x").outerText;
-		if (currentData != xEnd) {
-			document.getElementById("test-x").innerHTML = xEnd;
+	function updateMinutes(){
+		let minutes = date.getMinutes();
+		if (minutes < 10) minutes = '0' + minutes;
+		if (minutes != time.children[1].outerText) {
+			time.children[1].innerHTML = minutes;
 		}
-	});
+	}
+
+	function updateDate(){
+		let curDate = date.getDate();
+		if (curDate < 10) curDate = '0' + curDate;
+		if (curDate != myDate.children[0].outerText) {
+			myDate.children[0].innerHTML = curDate;
+		}
+	}
+
+	function updateMonth(){
+		let month = date.getMonth() + 1;
+		if (month < 10) month = '0' + month;
+		if (month != myDate.children[1].outerText) {
+			myDate.children[1].innerHTML = month;
+		}
+	}
+
+	function updateYear(){
+		let year = date.getFullYear();
+		if (year < 10) year = '0' + year;
+		if (year != myDate.children[2].outerText) {
+			myDate.children[2].innerHTML = year;
+		}
+	}
+
+	function updateWeek(){
+		let day = date.getDay();
+		let daysOfWeek = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+
+		if (week.children[0].outerText != daysOfWeek[day]) {
+			for (let i = 0; i < day; i++) {
+				daysOfWeek.push(daysOfWeek[0]);
+				daysOfWeek.shift();
+			}
+			for (let i = 0; i < daysOfWeek.length; i++) {
+				week.children[i].innerHTML = daysOfWeek[i];
+			}
+		}
+	}
+
+	function updateTest(){
+		Object.keys(x).forEach(function(el) {
+			let xEnd = String(x[el].timeEnd);
+			let currentData = document.getElementById("test-x").outerText;
+			if (currentData != xEnd) {
+				document.getElementById("test-x").innerHTML = xEnd;
+			}
+		});
+	}
 }
 
 setInterval(update, 100);
